c67e9493ad4aba658f1792cf6da04c43ed27fe7e
Hao0123299 <hao0123956@gmail.com>
2020-06-02 08:30:14 +0700

    file

---
+++ QuanLyPhuKienThoiTrang/DXApplication1/frmQuanLyPhuKienThoitrang/view/DangNhap.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace frmQuanLyPhuKienThoitrang.view
+{
+    partial class frmDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDangNhap = new DevExpress.XtraEditors.LabelControl();
+            this.lblTenDangNhap = new DevExpress.XtraEditors.LabelControl();
+            this.lblMatKhau = new DevExpress.XtraEditors.LabelControl();
+            this.txtDangNhap = new DevExpress.XtraEditors.TextEdit();
+            this.txtMatKhau = new DevExpress.XtraEditors.TextEdit();
+            this.lblHoi = new DevExpress.XtraEditors.LabelControl();
+            this.chEMatKhau = new DevExpress.XtraEditors.CheckEdit();
+            this.hLCDangKy = new DevExpress.XtraEditors.HyperlinkLabelControl();
+            this.btnDangNhap = new DevExpress.XtraEditors.SimpleButton();
+            this.btnThoat = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDangNhap.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtMatKhau.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chEMatKhau.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblDangNhap
+            // 
+            this.lblDangNhap.Appearance.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDangNhap.Appearance.Options.UseFont = true;
+            this.lblDangNhap.Location = new System.Drawing.Point(362, 47);
+            this.lblDangNhap.Name = "lblDangNhap";
+            this.lblDangNhap.Size = new System.Drawing.Size(59, 15);
+            this.lblDangNhap.TabIndex = 0;
+            this.lblDangNhap.Text = "Đăng nhập";
+            // 
+            // lblTenDangNhap
+            // 
+            this.lblTenDangNhap.Appearance.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTenDangNhap.Appearance.Options.UseFont = true;
+            this.lblTenDangNhap.Location = new System.Drawing.Point(209, 75);
+            this.lblTenDangNhap.Name = "lblTenDangNhap";
+            this.lblTenDangNhap.Size = new System.Drawing.Size(81, 15);
+            this.lblTenDangNhap.TabIndex = 0;
+            this.lblTenDangNhap.Text = "Tên đăng nhập";
+            // 
+            // lblMatKhau
+            // 
+            this.lblMatKhau.Appearance.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMatKhau.Appearance.Options.UseFont = true;
+            this.lblMatKhau.Location = new System.Drawing.Point(209, 112);
+            this.lblMatKhau.Name = "lblMatKhau";
+            this.lblMatKhau.Size = new System.Drawing.Size(51, 15);
+            this.lblMatKhau.TabIndex = 0;
+            this.lblMatKhau.Text = "Mật khẩu";
+            // 
+            // txtDangNhap
+            // 
+            this.txtDangNhap.Location = new System.Drawing.Point(320, 69);
+            this.txtDangNhap.Name = "txtDangNhap";
+            this.txtDangNhap.Size = new System.Drawing.Size(332, 20);
+            this.txtDangNhap.TabIndex = 1;
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Location = new System.Drawing.Point(320, 107);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(332, 20);
+            this.txtMatKhau.TabIndex = 1;
+            // 
+            // lblHoi
+            // 
+            this.lblHoi.Appearance.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHoi.Appearance.Options.UseFont = true;
+            this.lblHoi.Location = new System.Drawing.Point(209, 174);
+            this.lblHoi.Name = "lblHoi";
+            this.lblHoi.Size = new System.Drawing.Size(107, 15);
+            this.lblHoi.TabIndex = 0;
+            this.lblHoi.Text = "Bạn dã có tài khản ?";
+            // 
+            // chEMatKhau
+            // 
+            this.chEMatKhau.Location = new System.Drawing.Point(549, 170);
+            this.chEMatKhau.Name = "chEMatKhau";
+            this.chEMatKhau.Properties.Appearance.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chEMatKhau.Properties.Appearance.Options.UseFont = true;
+            this.chEMatKhau.Properties.Caption = "Nhớ mật khẩu";
+            this.chEMatKhau.Size = new System.Drawing.Size(103, 19);
+            this.chEMatKhau.TabIndex = 2;
+            // 
+            // hLCDangKy
+            // 
+            this.hLCDangKy.Location = new System.Drawing.Point(362, 174);
+            this.hLCDangKy.Name = "hLCDangKy";
+            this.hLCDangKy.Size = new System.Drawing.Size(40, 13);
+            this.hLCDangKy.TabIndex = 3;
+            this.hLCDangKy.Text = "Đăng ký";
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.Appearance.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangNhap.Appearance.Options.UseFont = true;
+            this.btnDangNhap.Location = new System.Drawing.Point(320, 220);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(75, 23);
+            this.btnDangNhap.TabIndex = 4;
+            this.btnDangNhap.Text = "Đăng nhập";
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Appearance.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Appearance.Options.UseFont = true;
+            this.btnThoat.Location = new System.Drawing.Point(577, 220);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 4;
+            this.btnThoat.Text = "Thoát";
+            // 
+            // frmDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 268);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnDangNhap);
+            this.Controls.Add(this.hLCDangKy);
+            this.Controls.Add(this.chEMatKhau);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.txtDangNhap);
+            this.Controls.Add(this.lblHoi);
+            this.Controls.Add(this.lblMatKhau);
+            this.Controls.Add(this.lblTenDangNhap);
+            this.Controls.Add(this.lblDangNhap);
+            this.Name = "frmDangNhap";
+            this.Text = "Đăng nhập";
+            ((System.ComponentModel.ISupportInitialize)(this.txtDangNhap.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtMatKhau.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chEMatKhau.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.LabelControl lblDangNhap;
+        private DevExpress.XtraEditors.LabelControl lblTenDangNhap;
+        private DevExpress.XtraEditors.LabelControl lblMatKhau;
+        private DevExpress.XtraEditors.TextEdit txtDangNhap;
+        private DevExpress.XtraEditors.TextEdit txtMatKhau;
+        private DevExpress.XtraEditors.LabelControl lblHoi;
+        private DevExpress.XtraEditors.CheckEdit chEMatKhau;
+        private DevExpress.XtraEditors.HyperlinkLabelControl hLCDangKy;
+        private DevExpress.XtraEditors.SimpleButton btnDangNhap;
+        private DevExpress.XtraEditors.SimpleButton btnThoat;
+    }
+}
